991aa19d4547bb65b8da7ee8ac9baded9781b559
Shkliarova <shklyarovaalisa@gmail.com>
2023-12-29 12:00:51 +0200

    hw 3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key{
+    private signature: number;
+
+    constructor(signature: number = Math.random()) {
+        this.signature = signature;
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person{
+    constructor(private key: Key){}
+
+    getKey(): Key{
+        return this.key;
+    }
+}
+
+abstract class House{
+    constructor(protected door: boolean, protected key: Key, protected tenants: Person[] = []){}
+
+    comeIn(person: Person): void{
+        if(this.door === true){
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House{
+    constructor(key: Key){
+        super(false, key);
+    }
+    openDoor(key: Key): void{
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+          } else {
+            this.door = false;
+          }
+        }
+    }
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
